c8a1f1f7b7eb34de5ae76dbb91a1c066d8a08fba
jamnemosyneb <jamellabunocan04@gmail.com>
2025-10-16 22:06:23 +0800

    Add files via upload
    Uploaded Java case problem and output screenshot.

---
+++ jamella bunocan/caseproblem.java
@@ -0,0 +1,18 @@
+public class caseproblem {
+    public static void main(String[] args) {
+        // Display personal information
+        System.out.println("Name: Jamella Bunocan");
+        System.out.println("ID Number: 2024-303303");
+        System.out.println("------------------------------");
+
+        
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            sum += i;  
+        }
+
+        System.out.println("Result: The sum of the first 10 natural numbers is " + sum);
+    }
+}
+
